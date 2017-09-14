12f10757ae832cf824634221c683de33985eab6b
KennethBrandon <kenwbrandon@gmail.com>
2017-09-13 23:19:01 -0700

    making GraphView 3 react to touch

--- app/src/main/java/com/redkb/graphviewdemo/GraphView3.java
+++ app/src/main/java/com/redkb/graphviewdemo/GraphView3.java
@@ -5,67 +5,121 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class GraphView3 extends View {
+import java.text.NumberFormat;
 
+public class GraphView3 extends View {
+    private static final String TAG = "GraphView3";
     private static final int STROKE_WIDTH_DP = 4;
+    private static final int POINTER_RADIUS_DP = 15;
+    private final int mStrokeInPx = (int) dpToPx(STROKE_WIDTH_DP);
+    private final int mPointerRadiusPx = (int) dpToPx(POINTER_RADIUS_DP);
     private final Paint mPaintLine = new Paint();
+    private final Paint mPaintPointer = new Paint();
+    private final Paint mPaintAxes = new Paint();
+    private final Paint mPaintText = new Paint();
     private final Path mPath = new Path();
+    private final NumberFormat mFormat = NumberFormat.getCurrencyInstance();
+    private Point mPointerLocation = new Point();
+    private boolean mDown = false;
+    private float mMaxValue;
+    private float mMinValue;
+    private int mMaxIndex;
+    private float[] mData;
+    private float mCurrentValue; //the value of the current down location
 
     public GraphView3(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
         mPaintLine.setColor(Color.argb(255, 33, 234, 152));
-        int strokeInPx = (int) dpToPx(STROKE_WIDTH_DP);
-        mPaintLine.setStrokeWidth(strokeInPx);
+        mPaintLine.setStrokeWidth(mStrokeInPx);
         mPaintLine.setStyle(Paint.Style.STROKE);
         mPaintLine.setAntiAlias(true);
         mPaintLine.setStrokeJoin(Paint.Join.ROUND);
+
+        mPaintPointer.setColor(Color.argb(255, 100, 20, 40));
+        mPaintPointer.setStrokeWidth(mStrokeInPx);
+        mPaintPointer.setStyle(Paint.Style.STROKE);
+        mPaintPointer.setAntiAlias(true);
+        mPaintPointer.setStrokeJoin(Paint.Join.ROUND);
+
+        mPaintAxes.setColor(Color.argb(255, 10, 120, 100));
+        mPaintAxes.setStrokeWidth(mStrokeInPx);
+        mPaintAxes.setStyle(Paint.Style.STROKE);
+        mPaintAxes.setAntiAlias(true);
+        mPaintAxes.setStrokeJoin(Paint.Join.ROUND);
+
+        mPaintText.setColor(Color.argb(255, 100, 20, 40));
+        mPaintText.setStrokeWidth(dpToPx(1));
+        mPaintText.setTextSize(dpToPx(20));
+        mPaintText.setAntiAlias(true);
     }
 
     public void setData(float[] data) {
         mPath.reset();
-        float max = findMax(data);
-        float min = findMin(data);
-        mPath.rMoveTo(getXFromIndex(0, data.length), getYFromValue(data[0], max, min));
+        mMaxValue = findMax(data);
+        mMinValue = findMin(data);
+        mData = data;
+        mMaxIndex = data.length;
+
+        //creating path from data
+        mPath.rMoveTo(getXFromIndex(0), getYFromValue(data[0]));
         for (int i = 1; i < data.length; i++) {
-            mPath.lineTo(getXFromIndex(i, data.length), getYFromValue(data[i], max, min));
+            mPath.lineTo(getXFromIndex(i), getYFromValue(data[i]));
         }
         invalidate();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
+        //Log.i(TAG, "Event: " + event.getAction() + " (" + event.getX() + ", " + event.getY() + ")");
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-
-                invalidate();
-                break;
-
             case MotionEvent.ACTION_MOVE:
-
+                setPointerLocation(event);
+                mDown = true;
                 invalidate();
                 break;
 
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
-
+                mDown = false;
                 invalidate();
                 break;
         }
-        return super.onTouchEvent(event);
+        return true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         drawAxes(canvas);
         drawLine(canvas);
+        if (mDown) {
+            drawPointerCircle(canvas);
+            drawPointerX(canvas);
+            drawTextValue(canvas);
+        }
         super.onDraw(canvas);
+    }
+
+    private void drawTextValue(Canvas canvas) {
+        canvas.drawText(mFormat.format(mCurrentValue), mPointerLocation.x + mPointerRadiusPx * 1.2f, mPointerLocation.y - 2 * mStrokeInPx, mPaintText);
+    }
+
+    private void drawPointerX(Canvas canvas) {
+        //X Marks the spot
+        canvas.drawLine(mPointerLocation.x, 0, mPointerLocation.x, canvas.getHeight(), mPaintPointer); //vertical line
+        canvas.drawLine(0, mPointerLocation.y, canvas.getWidth(), mPointerLocation.y, mPaintPointer); //horizontal line
+    }
+
+    private void drawPointerCircle(Canvas canvas) {
+        canvas.drawCircle(mPointerLocation.x, mPointerLocation.y, mPointerRadiusPx, mPaintPointer);
     }
 
     private void drawLine(Canvas canvas) {
@@ -73,16 +127,29 @@ public class GraphView3 extends View {
     }
 
     private void drawAxes(Canvas canvas) {
-        canvas.drawLine(0, 0, 0, canvas.getHeight(), mPaintLine);
-        canvas.drawLine(0, canvas.getHeight(), canvas.getWidth(), canvas.getHeight(), mPaintLine);
+        canvas.drawLine(0, 0, 0, canvas.getHeight(), mPaintAxes);
+        canvas.drawLine(0, canvas.getHeight(), canvas.getWidth(), canvas.getHeight(), mPaintAxes);
     }
 
-    private float getYFromValue(float value, float max, float min) {
-        return (max - value) * (getHeight()) / (max - min);
+    private float getYFromValue(float value) {
+        return (mMaxValue - value) * (getHeight()) / (mMaxValue - mMinValue);
     }
 
-    private float getXFromIndex(int i, int max) {
-        return ((float) i * ((getWidth()) / ((float) max - 1.0f)));
+    private float getXFromIndex(int i) {
+        return ((float) i * ((getWidth()) / ((float) mMaxIndex - 1.0f)));
+    }
+
+    private void setPointerLocation(MotionEvent event) {
+        //Using the width and the x of the event we find the corresponding data index
+        int index = (int) (((event.getX()) * (mMaxIndex - 1.0f) / ((float) getWidth())) + 1f);
+        if (index < 0) {
+            index = 0;
+        }
+        if (index > mMaxIndex - 1) {
+            index = mMaxIndex - 1;
+        }
+        mCurrentValue = mData[index];
+        mPointerLocation.set((int) getXFromIndex(index), (int) getYFromValue(mCurrentValue));
     }
 
     private float dpToPx(final float value) {
